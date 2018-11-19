2a8eab63eefa6aad20423631dd1eef8cc2687db9
Luiz Guilherme F Palazzo <luiz1213@hotmail.com>
2018-11-19 15:13:56 -0200

    ajustes

---
+++ src/main/resources/inserts.sql
@@ -0,0 +1,3 @@
+INSERT INTO jobsystem_team(ID, EMAIL, NAME) VALUES (seq_team.nextval, 'producao@empresa.com', 'PRODUÇÃO' );
+INSERT INTO jobsystem_team(ID, EMAIL, NAME) VALUES (seq_team.nextval, 'incidentes1@empresa.com', 'INCIDENTES 1' );
+INSERT INTO jobsystem_team(ID, EMAIL, NAME) VALUES (seq_team.nextval, 'incidentes2@empresa.com', 'INCIDENTES 2' );
